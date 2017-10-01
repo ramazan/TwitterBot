d1c0e36898d75fb1b3b9426a26c927bfbddd4fe9
Ramazan Demir <ramazan.demir@sahibinden.com>
2017-10-01 20:45:41 +0300

    add search keyword sample usages

--- bot.js
+++ bot.js
@@ -1,10 +1,23 @@
-console.log('Twitter bot is starting..');
+console.log('Twitter bot is starting..\n');
 
 var Twit = require('twit');  // reference twit module..
 
 var configuration = require('./config'); // reference config.js file
 
-console.log(configuration);
-
 var T = new Twit(configuration);
 
+var params = {
+    q : 'Fenerbahçe',
+    count : 5
+}
+
+T.get('search/tweets', params ,gotData);
+
+function gotData(err,data,response){
+    
+    var tweets = data.statuses;
+
+    tweets.forEach(function(element) {
+        console.log(element.text);
+    }, this);
+}
